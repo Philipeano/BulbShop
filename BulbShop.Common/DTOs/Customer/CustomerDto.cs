ac6414f9b5f26654c337e51de68bdbf76c157ff7
Philip Newman <philipeano@gmail.com>
2024-03-06 12:31:08 +0100

    Implement customer repository

---
+++ BulbShop.Common/DTOs/Customer/CustomerDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulbShop.Common.DTOs.Customer
+{
+    public record CustomerDto : BaseCustomerModel
+    {
+        public Guid Id { get; set; }
+
+    }
+}
